9c1476617db5fbd78ff17f67c6804286c09da537
Emma Oyetey <emmaflookis@hotmail.com>
2024-07-19 17:02:12 +0100

    added commets to digital projets

--- src/Data/Digital.ts
+++ src/Data/Digital.ts
@@ -9,7 +9,7 @@ const Project : DigitalType[] = [
         codeLink: "https://github.com/nology-tech/wov-client-project/blob/main/README.md",
         githubLink: "https://github.com/nology-tech/wov-client-project",
         skills: ["React", "SCSS",],
-        more:"",
+        more:"add blurb , add blurb. aufoaiuf oaiufoaifuoa ifu aoifu oaifuoaiuf oaiufoaiufoaiu foaiu fao ifuoaiufoaiufoaifu oaif aoisf aoifu aoisfu oaisfu aoisfu oaisf oaifu oaisfu oaif oaifus oaif oaif oaf oaif oaisf oaisf oaiuf oafoafoaifoaif aoifu aoifu aoifu aoif oaifu oaisf oaisfu aoisf aoisfu oais ageil working. pair programming. being vulnerable and a learner. open to learnign fromothers honest about own capabilities and needs and others stregths..",
       },
       {
         name: "Hangman",
@@ -19,7 +19,7 @@ const Project : DigitalType[] = [
         codeLink: "https://github.com/EmmaOyetey/hangman",
         githubLink: "https://github.com/EmmaOyetey/hangman",
         skills: ["React", "SCSS",],
-        more:"",
+        more:"Hangman is hangman! A classic game which was a great introudction to Java programming and OOP. I enjoyed incorprtaing the ascii elements of this game and ensuring the ongoing display of previous guesses as would be seen in a pen and paper version. Its all about arrays here.. who would have thought you could appreciate the importance of an array of items in logic thinking! ",
       },
       {
         name: "Calculator",
@@ -28,8 +28,8 @@ const Project : DigitalType[] = [
         demoVideo: "../../public/digitalProjects/calculatorDemo.mp4",
         codeLink: "https://github.com/EmmaOyetey/calculator/tree/main",
         githubLink: "https://emmaoyetey.github.io/calculator/",
-        skills: ["React", "SCSS",],
-        more:"",
+        skills: ["Vite", "Typescript", "SCSS",],
+        more:"Baught up in 80's, calculators were going to make us stupid.. now we are facing AI. Does AI make us redundant or just force us to change the way we do things and use our energies. I'm interested to explore how tech and AI can transoform our education systems for the better. As for the calculator it got me thinking about BODMAS. How do we teach concepts through our design & content and how do our users interactions inform our content % design? to BODMAS or not to BODMAS?!!! ",
       },
       {
         name: "TuneTeezr",
@@ -38,8 +38,8 @@ const Project : DigitalType[] = [
         demoVideo: "../../public/digitalProjects/tuneTeezrDemo.mp4",
         codeLink: "https://github.com/EmmaOyetey/game",
         githubLink: "https://emmaoyetey.github.io/game/",
-        skills: ["Typescript", "Vite", "SCSS", "DOM"],
-        more:"",
+        skills: ["Typescript", "Vite", "SCSS", "DOM manipulation", "Mobile First Design"],
+        more:"Inspired by heardle, players guess the artist or band based on the song's intro. Creating accessible education content has been a passion in previous roles, this has influenced my approach to the design of this game. I've used simple reveals and animations (pulse) to guide users through, alongside audio to communicate incorrect guesses and the event of a win or loss. Avoiding wordy instructions...",
       },
       {
         name: "Raven",
@@ -48,8 +48,8 @@ const Project : DigitalType[] = [
         demoVideo: "../../public/digitalProjects/ravenDemo.mp4",
         codeLink: "https://github.com/EmmaOyetey/game",
         githubLink: "https://emmaoyetey.github.io/game/",
-        skills: ["Java", "Typescript", "Vite", "SCSS", "Spring", "MySQL"],
-        more:"Inspired by my work in the charitable sector, and my passion for education and working alongside young people.This project aims to bridge the gap between organisations and those seeking assistance.Providing the opportunity to easily find and connect with the services they need and want..",
+        skills: ["Java", "Typescript", "Vite", "React", "SCSS", "Spring", "Spring Boot", "MySQL"],
+        more:"Drawing on my experience in both the charitable and education sectors, Raven was inspired by the need for charitable services to be more accessible, whilst also providing transparency regarding the quality and impact of the support offered. Giving education providers the opportunity to easily find and connect with the services they need and want...",
       },
     ];
     
