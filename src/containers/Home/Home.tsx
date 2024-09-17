5fd0b9874f3eb870eb74cc9b5ad669049ebe5f6b
Emma Oyetey <emmaflookis@hotmail.com>
2024-09-17 14:23:13 +0100

    simplifying scss for landing page to resolve deploy issues

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "./Home.scss";
 import About from "../About/About";
 import ExperienceCarousel from "../ExperienceCarousel/ExperienceCarousel";
@@ -8,25 +7,6 @@ import NavBar from "../../components/Nav/Nav";
 import ScrollArrow from "../../components/ScrollArrow/ScrollArrow";
 
 const Home = () => {
-  useEffect(() => {
-    const handleScroll = () => {
-      const aboutMe = document.querySelector(".home__about-me");
-      if (aboutMe) {
-        const aboutMeTop = aboutMe.getBoundingClientRect().top;
-        const aboutMeVisible = aboutMeTop <= window.innerHeight / 2;
-
-        if (aboutMeVisible) {
-          aboutMe.classList.add("visible");
-        } else {
-          aboutMe.classList.remove("visible");
-        }
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="home">
       <div className="home__landing-page">
@@ -35,7 +15,7 @@ const Home = () => {
         <ScrollArrow />
       </div>
 
-      <div id="about-me" className="home__about-me">
+      <div id="about-me" className="home__about-me visible">
         <div className="home__about-me--content">
           <h2 className="home__section__header home__section__header--about-me">--- ME ---</h2>
           <div className="home__content-section home__content-section--about">
@@ -44,7 +24,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div id="where" className="home__section home__section--where">
+      <div id="where" className="home__section home__section--where visible">
         <h2 className="home__section__header home__section__header--where">--- WHERE ---</h2>
         <div className="home__section__content home__section__content--where">
           <Where />
@@ -53,7 +33,7 @@ const Home = () => {
 
       <div id="what">
         <div className="home__what-style-container">styling for a tag</div>
-        <div className="home__section home__section--what">
+        <div className="home__section home__section--what visible">
           <p className="home__section__header home__section__header--what">--- WHAT ---</p>
           <div className="home__section__content home__section__content--what">
             <ExperienceCarousel />
@@ -61,7 +41,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div id="who" className="home__section home__section--who">
+      <div id="who" className="home__section home__section--who visible">
         <p className="home__section__header home__section__header--who">--- WHO ---</p>
         <div className="home__section__content home__section__content--who">
           <PeopleCarousel />
@@ -74,6 +54,83 @@ const Home = () => {
 };
 
 export default Home;
+
+// import { useEffect } from "react";
+// import "./Home.scss";
+// import About from "../About/About";
+// import ExperienceCarousel from "../ExperienceCarousel/ExperienceCarousel";
+// import PeopleCarousel from "../PeopleCarousel/PeopleCarousel";
+// import Where from "../Where/Where";
+// import NavBar from "../../components/Nav/Nav";
+// import ScrollArrow from "../../components/ScrollArrow/ScrollArrow";
+
+// const Home = () => {
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const aboutMe = document.querySelector(".home__about-me");
+//       if (aboutMe) {
+//         const aboutMeTop = aboutMe.getBoundingClientRect().top;
+//         const aboutMeVisible = aboutMeTop <= window.innerHeight / 2;
+
+//         if (aboutMeVisible) {
+//           aboutMe.classList.add("visible");
+//         } else {
+//           aboutMe.classList.remove("visible");
+//         }
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
+
+//   return (
+//     <div className="home">
+//       <div className="home__landing-page">
+//         <h1 className="home__landing-page__name">Emma Oyetey</h1>
+//         <h3 className="home__landing-page__overview">github.com/EmmaOyetey</h3>
+//         <ScrollArrow />
+//       </div>
+
+//       <div id="about-me" className="home__about-me">
+//         <div className="home__about-me--content">
+//           <h2 className="home__section__header home__section__header--about-me">--- ME ---</h2>
+//           <div className="home__content-section home__content-section--about">
+//             <About />
+//           </div>
+//         </div>
+//       </div>
+
+//       <div id="where" className="home__section home__section--where">
+//         <h2 className="home__section__header home__section__header--where">--- WHERE ---</h2>
+//         <div className="home__section__content home__section__content--where">
+//           <Where />
+//         </div>
+//       </div>
+
+//       <div id="what">
+//         <div className="home__what-style-container">styling for a tag</div>
+//         <div className="home__section home__section--what">
+//           <p className="home__section__header home__section__header--what">--- WHAT ---</p>
+//           <div className="home__section__content home__section__content--what">
+//             <ExperienceCarousel />
+//           </div>
+//         </div>
+//       </div>
+
+//       <div id="who" className="home__section home__section--who">
+//         <p className="home__section__header home__section__header--who">--- WHO ---</p>
+//         <div className="home__section__content home__section__content--who">
+//           <PeopleCarousel />
+//         </div>
+//       </div>
+
+//       <NavBar />
+//     </div>
+//   );
+// };
+
+// export default Home;
 
 // import { useEffect } from "react";
 // import "./Home.scss";
